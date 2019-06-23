ac5a646c305f7ad28d3eeae71babfa0a4bb3a14a
Yevgen Sereda <yevgen.sereda@jelvix.com>
2019-06-24 01:06:13 +0300

    work started

---
+++ js/main.js
@@ -0,0 +1,7 @@
+import merge from './modules/common';
+// import FixedHeader from './modules/fixed';
+import CalcForm from './modules/calcForm';
+// init
+document.addEventListener('DOMContentLoaded', function() {
+	let calcForm = new CalcForm;
+});
---
+++ js/modules/calcForm.js
@@ -0,0 +1,63 @@
+function CalcForm(options) {
+    let inputs = document.querySelectorAll('[data-calc]');
+    let totalValueField = document.querySelectorAll('.total-value');
+    let totalValue = 0;
+    
+    inputs.forEach((el) => {
+        // let value = +el.dataset.calcValue;
+
+        // if (el.checked) {
+        //     totalValue += value;
+        // };
+
+        el.addEventListener('change', function() {
+            totalValue = 0;
+            receiveDataFromChecked(inputs);
+            updateTotalValue(totalValue);
+        });
+
+        receiveDataFromChecked(inputs);
+
+        updateTotalValue(totalValue);
+    });
+
+    function updateTotalValue(totalValue) {
+        return totalValueField[0].innerHTML = `${totalValue}$`;
+    };
+
+    function receiveDataFromChecked(elements) {
+        let elems = Array.prototype.slice.call(elements);
+        let checked = elems.filter((item) => {
+            return item.checked;
+        });
+        checked.forEach((elem) => {
+            let value = +elem.dataset.calcValue;
+            let title = elem.dataset.calcTitle;
+            totalValue += value;
+            createRow(title, value);
+            return totalValue;
+        })
+    };
+
+    function cleanRows() {
+        let holder = document.querySelector('.sidebar-block');
+        holder.innerHTML = '';
+    }
+
+    function createRow(name, val) {
+        let holder = document.querySelector('.sidebar-block');
+        let row = document.createElement('div');
+        let titleBlock = document.createElement('div');
+        let valueBlock = document.createElement('div');
+        row.classList.add('sidebar-row');
+        titleBlock.classList.add('title');
+        valueBlock.classList.add('value');
+        titleBlock.innerHTML = name;
+        valueBlock.innerHTML = val;
+        row.appendChild(titleBlock);
+        row.appendChild(valueBlock);
+        holder.appendChild(row);
+    }
+}
+
+export default CalcForm;
