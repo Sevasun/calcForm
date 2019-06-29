6e800d18ac66f18538f6415651069bea1b8c8cff
Yevgen Sereda <yevgen.sereda@jelvix.com>
2019-06-29 23:30:52 +0300

    change markup

--- js/main.js
+++ js/main.js
@@ -8,38 +8,38 @@
 
 // jquery version
 
-jQuery(function() {
-	calcForm();
-});
+// jQuery(function() {
+// 	calcForm();
+// });
 
-function calcForm() {
-	let inputs = $('[data-calc]');
-	inputs.each(function(i) {
-		let title = $(this).attr("data-calc-title");
-		let value = $(this).attr('data-calc-value');
+// function calcForm() {
+// 	let inputs = $('[data-calc]');
+// 	inputs.each(function(i) {
+// 		let title = $(this).attr("data-calc-title");
+// 		let value = $(this).attr('data-calc-value');
 
-		$(this).attr('data-calc-input-id', i);
+// 		$(this).attr('data-calc-input-id', i);
 
-		if($(this).is(':checked')) {
-			createRow($(this).attr('data-calc-input-id'), title, value);
-		}
+// 		if($(this).is(':checked')) {
+// 			createRow($(this).attr('data-calc-input-id'), title, value);
+// 		}
 
-		$(this).on('change', function() {
-			let radioName = $(this).attr('name');
-			$(`[name=${radioName}]`).each(function() {
-				let currentId = $(this).attr('data-calc-input-id');
-				if ($(this).is(':not(:checked)')) {
-					$($(`.row[data-calc-id=${currentId}]`)).remove();
-				}
-			});
-			createRow($(this).attr('data-calc-input-id'), title, value);
-		});
-	});
+// 		$(this).on('change', function() {
+// 			let radioName = $(this).attr('name');
+// 			$(`[name=${radioName}]`).each(function() {
+// 				let currentId = $(this).attr('data-calc-input-id');
+// 				if ($(this).is(':not(:checked)')) {
+// 					$($(`.row[data-calc-id=${currentId}]`)).remove();
+// 				}
+// 			});
+// 			createRow($(this).attr('data-calc-input-id'), title, value);
+// 		});
+// 	});
 
-	function createRow(id, title, value) {
-		$('.sidebar-block').append(`<div data-calc-id="${id}" class="row">
-			<strong class="title">${title}</strong>
-			<strong class="value">$${value}</strong>
-		</div>`)
-	};
-}
+// 	function createRow(id, title, value) {
+// 		$('.sidebar-block').append(`<div data-calc-id="${id}" class="row">
+// 			<strong class="title">${title}</strong>
+// 			<strong class="value">$${value}</strong>
+// 		</div>`)
+// 	};
+// }
